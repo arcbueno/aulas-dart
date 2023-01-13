a24e941c2bfd64ef8f42b41ab3837c0e7072d7b3
Pedro Bueno <pedroarcbueno@gmail.com>
2023-01-12 21:24:33 -0300

    exemplo com for

--- aula5/try_catch.dart
+++ aula5/try_catch.dart
@@ -1,24 +1,49 @@
 import 'dart:io';
 
 void main() {
-  try {
-    // int? idade;
-    // print('Minha idade agora é ${idade! + 1}');
-
-    // var cep = "50.870-540";
-    var cep = "eu amo rebeca";
-    var cepSplitted = cep.split('.');
-    print(cepSplitted);
-    var initialNumbers = cepSplitted[0];
-    print(initialNumbers);
-    int initialNumbersParsed = int.parse(initialNumbers);
-    print(initialNumbersParsed);
-  } catch (error) {
-    print('Digite um valor válido');
-  }
-
-  print('Continue o código...');
+  do {
+    try {
+      print('Digite um valor por favor');
+      var input = stdin.readLineSync();
+      var inputInt = int.parse(input!);
+      print('Meu valor inserido é $inputInt');
+      break;
+    } catch (e) {
+      print('Digite um valor inteiro válido');
+      continue;
+    }
+  } while (true);
 }
+
+
+
+
+
+
+
+
+
+
+
+// void main() {
+//   try {
+//     // int? idade;
+//     // print('Minha idade agora é ${idade! + 1}');
+
+//     // var cep = "50.870-540";
+//     var cep = "eu amo rebeca";
+//     var cepSplitted = cep.split('.');
+//     print(cepSplitted);
+//     var initialNumbers = cepSplitted[0];
+//     print(initialNumbers);
+//     int initialNumbersParsed = int.parse(initialNumbers);
+//     print(initialNumbersParsed);
+//   } catch (error) {
+//     print('Digite um valor válido');
+//   }
+
+//   print('Continue o código...');
+// }
 
 // void main(List<String> args) {
 //   int? idade;
